c279d612b00b9cd9c108101af1e0a76803a561d0
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-20 13:57:31 +0100

    Act

--- CuentaBanco/src/CuentaBanco.java
+++ CuentaBanco/src/CuentaBanco.java
@@ -2,14 +2,14 @@ public class CuentaBanco {
     private int saldo;
     private String password;
 
-    public CuentaBanco (int saldoInicial, String password){
+    public CuentaBanco (int saldoInicial, String password){// objeto generico
         this.saldo=saldoInicial;
         this.password=password;
     }
 
-    public int retirar (int cantidad, String passwordIntro){
+    public int retirar (int cantidad, String passwordIntro){ //metod Sacar dinero
 
-        if (password.equals(passwordIntro)){
+        if (password.equals(passwordIntro)){ // tras hacer el metodo paswordCorrecta podemos meterle (paswordCorrecta(passwordIntro)) en el parentesis
             if (saldo>=cantidad){
                 saldo-=cantidad;
             }else
@@ -18,7 +18,9 @@ public class CuentaBanco {
         return -2; // si la contraseña es falsa
     }
 
-    public boolean
+    public boolean passwordCorrecta ( String passwordIntro) {
+        return password.equals(passwordIntro);
+    }
 
     public static void main(String[] args) {
 
