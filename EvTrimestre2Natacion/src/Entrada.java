19dba4bb4bc6d6b95df0bb0fd801a4160e45c884
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-02-04 17:13:43 +0100

    añadido test

--- EvTrimestre2Natacion/src/Entrada.java
+++ EvTrimestre2Natacion/src/Entrada.java
@@ -4,6 +4,7 @@ public class Entrada {
     public static void main(String[] args) {
         Nadador nadador1 = new Nadador();//fecha con comas
         Nadador nadador2 = new Nadador("Sullivan", "789456m",(int)(Math.random()*999)+1,0.00f, 0.00f);
+        Nadador nadador3 = new Nadador("PRUEBILAN", "789456m",(int)(Math.random()*999)+1,0.00f, 0.00f);
 
     /*
     nadador2.mostrarDatos();
--- EvTrimestre2Natacion/test/model/NadadorTest.java
+++ EvTrimestre2Natacion/test/model/NadadorTest.java
@@ -1,12 +1,37 @@
 package model;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NadadorTest {
-@Test
-    void test(){
 
-}
+
+    Nadador nadadorPrueba;//creo nadador sin declararlo
+
+    @BeforeAll
+
+    public static void inicioPruebas() {
+        System.out.println("Iniciando las pruebas de clase");
+    }
+
+    @AfterAll
+    public static void finPruebas() {
+        System.out.println("IFinalizando las pruebas de clase");
+    }
+
+    @BeforeEach
+    public void crearNadador() {// aqui lo declaro lo creado arriba
+        nadadorPrueba = new Nadador("Probador", "Pruebas", 25, 7.00f, 2.50f);
+        System.out.println("Probando el beforeEach (en ecliplse es before)");
+    }
+
+    @AfterEach
+    public void borraNadador() {
+        nadadorPrueba=null;
+        System.out.println("Fin pueba del metodo");
+    }
 }
