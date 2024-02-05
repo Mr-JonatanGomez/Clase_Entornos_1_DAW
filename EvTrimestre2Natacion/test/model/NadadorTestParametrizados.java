e4d17e7fa5a326b196908a6d067776f3a9990aa1
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-02-05 16:20:01 +0100

    añadido test

--- EvTrimestre2Natacion/test/model/NadadorTestParametrizados.java
+++ EvTrimestre2Natacion/test/model/NadadorTestParametrizados.java
@@ -8,22 +8,22 @@ import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NadadorTestParametrizados2 {
+public class NadadorTestParametrizados {
 
     @ParameterizedTest
     @MethodSource("provideNadadores")
     public void testPuntuacionFinal(Nadador nadador, float expectedPuntuacionFinal) {
         float actualPuntuacionFinal = nadador.puntuacionNadador(nadador.getPuntuacionEjercicio(), nadador.getDificultadEjercicio());
-        assertEquals(expectedPuntuacionFinal, actualPuntuacionFinal, 0.01f);
-        //delta es el margen de error aceptable
+        assertEquals(expectedPuntuacionFinal, actualPuntuacionFinal, 0.01f,"Fallo");
     }
 
     private static Collection<Object[]> provideNadadores() {
         return Arrays.asList(new Object[][]{
-                {new Nadador("Nadador1", 7.50f, 3.20f), 24.00f,"HA FALLADO"},
+                {new Nadador("Nadador1", 7.50f, 3.20f), 24.00f},
                 {new Nadador("Nadador2", 7.50f, 3.00f), 22.50f},
                 {new Nadador("Nadador3", 5.00f, 3.00f), 15.00f},
                 {new Nadador("Nadador4", 10.0f, 3.00f), 30.00f}
+                //                    (Nombre,puntuacionEjercicio,dificultadEjercicio), resultado esperado
         });
     }
 }
